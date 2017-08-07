aab5df8e18a059ac79c62743a5fd38977e03e025
LiuYongfeng <lyf198511@l16.com>
2017-08-08 01:09:39 +0800

    ardcastBestPriactice2

---
+++ BoardcastBestPriactice2/app/src/main/java/com/example/administrator/boardcastbestpriactice2/BaseActivity.java
@@ -0,0 +1,72 @@
+package com.example.administrator.boardcastbestpriactice2;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.pm.ActivityInfoCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/7/24.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+
+    private ForceOfflineReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.BroadcastBestPractice2.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(receiver != null) {
+            unregisterReceiver(receiver);
+        }
+    }
+
+    class  ForceOfflineReceiver extends BroadcastReceiver{
+        private static final String TAG = "ForceOfflineReceiver";
+        @Override
+        public void onReceive(final Context context, final Intent intent){
+            Log.d(TAG, "onReceive: ForceOfflineReceiver received");
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setCancelable(false);
+            builder.setMessage("You are forced to be offline,please try to login again!");
+            builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();
+                    Intent intent1 = new Intent(context,LoginAcitity.class);
+                    context.startActivity(intent1);
+                }
+            });
+        }
+    }
+
+}
--- BoardcastBestPriactice2/app/src/main/java/com/example/administrator/boardcastbestpriactice2/MainActivity.java
+++ BoardcastBestPriactice2/app/src/main/java/com/example/administrator/boardcastbestpriactice2/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.administrator.boardcastbestpriactice2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";//logt + TAB
+    private Button forceOffline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        forceOffline = (Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent("com.example.BroadcastBestPractice2.FORCE_OFFLINE");
+                sendBroadcast(intent);
+                Log.d(TAG, "onClick: send intent com.example.BroadcastBestPractice2.FORCE_OFFLINE"); //logd + TAB
+            }
+        });
     }
 }
