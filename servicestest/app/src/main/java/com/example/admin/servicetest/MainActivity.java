246fe4dcdd983856bc0930843dbc65efc44f38b8
LiuYongfeng <lyf198511@163.com>
2017-08-17 19:57:03 +0800

    ServiceTest chapter8: 前台服务

--- servicestest/app/src/main/java/com/example/admin/servicetest/MainActivity.java
+++ servicestest/app/src/main/java/com/example/admin/servicetest/MainActivity.java
@@ -9,22 +9,25 @@ import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    private static final String TAG = "MainActivity";
     private MyService.DownloadBinder downloadBinder;
-
+    //让MainActivity和MyService之间建立关联,这里我们首先创建了一个ServiceConnection的匿名类
     private ServiceConnection connection = new ServiceConnection() {
         @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        //Activity与Service建立关联的时候调用
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
             downloadBinder = (MyService.DownloadBinder) service;
             downloadBinder.startDownload();
             downloadBinder.getProgress();
         }
 
         @Override
+        //Activity与Service解除关联的时候调用
         public void onServiceDisconnected(ComponentName componentName) {
 
         }
@@ -42,22 +45,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bindService.setOnClickListener(this);
         Button unbindService = (Button)findViewById(R.id.unbind_service);
         unbindService.setOnClickListener(this);
+        Log.d(TAG, "onCreate: MainActivity thread id is " + Thread.currentThread().getId());
     }
 
     @Override
     public void onClick(View v){
         switch (v.getId()){
             case R.id.start_service:
+                //通过intend在activity中启动MyService服务
                 Intent start_intend = new Intent(this,MyService.class);
+                //调用startService()方法来启动MyService
                 startService(start_intend);
                 break;
             case R.id.stop_service:
+                //通过intend在activity中停止MyService服务，
+                // 注意，每个service都要在AndroidManifest.xml中注册
                 Intent stop_intend = new Intent(this,MyService.class);
+                //调用stopService()方法来停止MyService
                 stopService(stop_intend);
                 break;
             case R.id.bind_service:
+                Intent bindIntent = new Intent(this,MyService.class);
+                //BIND_AUTO_CREATE表示在Activity和Service建立关联后自动创建Service，
+                // 这会使得MyService中的onCreate()方法得到执行，但onStartCommand()方法不会执行。
+                bindService(bindIntent,connection,BIND_AUTO_CREATE);
                 break;
             case R.id.unbind_service:
+                unbindService(connection);
                 break;
             default:
                 break;
--- servicestest/app/src/main/java/com/example/admin/servicetest/MyService.java
+++ servicestest/app/src/main/java/com/example/admin/servicetest/MyService.java
@@ -1,54 +1,103 @@
 package com.example.admin.servicetest;
 
-import android.app.DownloadManager;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-//实现后台下载功能，在activity中可以决定何时开始下载，以及随时查看下载进展。
+
+import static com.example.admin.servicetest.R.*;
+/*
+http://blog.csdn.net/guolin_blog/article/details/11952435
+*/
+
+//Service其实是运行在主线程里的（跟MainActivity是同一个进展）,耗时的过程最好开子进程进行处理，
+// 避免阻塞主进程,导致ANR。
+// 实现后台下载功能，在activity中可以决定何时开始下载，以及随时查看下载进展。
 public class MyService extends Service {
     private static final String TAG = "MyService";
 
     //创建一个DownloadBinder实例
     private DownloadBinder mBinder = new DownloadBinder();
-    //创建一个DownloadBinder类
-    class DownloadBinder extends Binder{
-        //开始下载
-        public void startDownload(){
-            Log.d(TAG, "startDownload: executed");
-        }
-        //查看进度
-        public int getProgress(){
-            Log.d(TAG, "getProgress: executed");
-            return 0;
-        }
-    }
 
     public MyService() {
     }
-
+    // onCreate()方法只在Service第一次被创建的时候调用，
+    // 如果当前Service已经被创建过了，不管怎样调用startService()方法，onCreate()方法都不会再执行
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onCreate(){
         super.onCreate();
-        Log.d(TAG, "onCreate: ");
+
+        //创建通知
+        Notification notification = new Notification(drawable.ic_launcher,
+                "有通知到来", System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+        notification.setLatestEventInfo(this, "这是通知的标题", "这是通知的内容",
+                pendingIntent);
+        startForeground(1, notification);
+        Log.d(TAG, "onCreate() executed");
+        Log.d(TAG, "onCreate: MyService thread id is " + Thread.currentThread().getId());
     }
 
     @Override
     public int onStartCommand(Intent intent,int flag,int startId){
-        Log.d(TAG, "onStartCommand: ");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "onStartCommand: ");
+            }
+        }).start();
+
         return super.onStartCommand(intent,flag,startId);
     }
 
     @Override
     public void onDestroy(){
+        //一个Service必须要在既没有和任何Activity关联又处理停止状态的时候才会被销毁。
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
     }
-
+    //这个onBind方法其实就是用于和Activity建立关联的
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         return mBinder;
+    }
+
+    //新增了一个MyBinder类继承自Binder类，然后在MyBinder中添加了一个startDownload()方法用于在后台执行下载任务
+    class DownloadBinder extends Binder{
+
+        public void startDownload(){
+            //执行具体的下载任务
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "startDownload: executed");
+                }
+            }).start();
+        }
+
+        public int getProgress(){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    //查看下载进度
+                    Log.d(TAG, "getProgress: executed");
+                }
+            }).start();
+            return 0;
+        }
     }
 }
