db5a7d8068f5f7788ecc2a93d88837981ab1703e
LiuYongfeng <lyf198511@163.com>
2017-08-24 16:29:48 +0800

    获取地址

--- LBSTest/app/src/main/java/com/example/administrator/lbstest/MainActivity.java
+++ LBSTest/app/src/main/java/com/example/administrator/lbstest/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.administrator.lbstest;
 
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LBSTest";
     public LocationClient mLocationClient;
     private TextView positionText;
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mLocationClient.stop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +41,17 @@ public class MainActivity extends AppCompatActivity {
         positionText = (TextView)findViewById(R.id.position_text_view);
         List<String> permissionList = new ArrayList<>();
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.
-                permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-                permissionList.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission
+                .ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
-        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.
-                permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED){
-            permissionList.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission
+                .READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
-        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.
-                permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-            permissionList.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission
+                .WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
 
         if(!permissionList.isEmpty()){
@@ -51,7 +63,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void requestLocation(){
+        initLocation();
         mLocationClient.start();
+    }
+
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setScanSpan(2000);//位置更新频率
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setIsNeedAddress(true);//获取地理位置
+        mLocationClient.setLocOption(option);
     }
 
     @Override
@@ -62,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 if(grantResults.length > 0){
                     for(int result : grantResults){
                         if(result != PackageManager.PERMISSION_GRANTED){
+                            Log.d(TAG, "onRequestPermissionsResult: " + result);
                             Toast.makeText(this,"必须同意所有权限才能使用本程序",Toast.LENGTH_SHORT).show();
                             finish();
                             return;
@@ -81,8 +103,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceiveLocation(BDLocation location){
             StringBuilder currentPosition = new StringBuilder();
-            currentPosition.append("维度:").append(location.getLatitude()).append("\n");
+            currentPosition.append("纬度:").append(location.getLatitude()).append("\n");
             currentPosition.append("经度:").append(location.getLongitude()).append("\n");
+            currentPosition.append("国家:").append(location.getCountry()).append("\n");
+            currentPosition.append("省:").append(location.getProvince()).append("\n");
+            currentPosition.append("市:").append(location.getCity()).append("\n");
+            currentPosition.append("区:").append(location.getDistrict()).append("\n");
+            currentPosition.append("街道:").append(location.getStreet()).append("\n");
             currentPosition.append("定位方式:");
             if(location.getLocType() == BDLocation.TypeGpsLocation){
                 currentPosition.append("GPS").append("\n");
