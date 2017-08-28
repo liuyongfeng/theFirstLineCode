da67e08b5461edd15b06bb78bb5a26f56499844c
LiuYongfeng <lyf198511@163.com>
2017-08-28 17:54:52 +0800

    CoolWeather init

---
+++ CoolWeather/app/src/main/java/com/example/admin/coolweather/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.admin.coolweather;
+/**
+ *  https://console.heweather.com/my/service
+ *  个人认证key: 74e15c6ca6cc4d9fa1ffd044399353a2
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
