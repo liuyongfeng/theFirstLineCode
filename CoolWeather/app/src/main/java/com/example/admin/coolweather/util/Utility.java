013b1cdc4832eec13576f9bde952fa56881ccfb1
LiuYongfeng <lyf198511@163.com>
2017-09-01 17:49:22 +0800

    CoolWeather add weather activity

--- CoolWeather/app/src/main/java/com/example/admin/coolweather/util/Utility.java
+++ CoolWeather/app/src/main/java/com/example/admin/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.admin.coolweather.db.City;
 import com.example.admin.coolweather.db.Country;
 import com.example.admin.coolweather.db.Province;
+import com.example.admin.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -83,5 +85,18 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
